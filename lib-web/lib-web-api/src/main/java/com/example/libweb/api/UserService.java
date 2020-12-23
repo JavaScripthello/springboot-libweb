03b5a5f7cf55df0c6b009091a18e603bc6947457
kw <2713445913@qq.com>
2020-12-23 22:37:49 +0800

    feat version1.0 init commit

---
+++ lib-web/lib-web-api/src/main/java/com/example/libweb/api/UserService.java
@@ -0,0 +1,14 @@
+package com.example.libweb.api;
+
+
+import com.example.libweb.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
